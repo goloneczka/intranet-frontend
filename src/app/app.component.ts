8bc8633966ada4c11b0eb6eeefea0d17a1301a0a
goloneczka <48791689+goloneczka@users.noreply.github.com>
2023-09-25 13:52:53 +0200

    feature_1 dynamic render home, employee and docs content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'intranet-app';
+  contentType = 'home';
+
+  contentTypeChange(value: string) {
+    this.contentType = value
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,19 @@ import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatDividerModule} from "@angular/material/divider";
+import { LeftNavComponent } from './component/left-nav/left-nav.component';
+import { ContentHomeComponent } from './component/content-home/content-home.component';
+import { ContentEmployeeComponent } from './component/content-employee/content-employee.component';
+import { ContentDocumentComponent } from './component/content-document/content-document.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LeftNavComponent,
+    ContentHomeComponent,
+    ContentEmployeeComponent,
+    ContentDocumentComponent,
   ],
   imports: [
     BrowserModule,
